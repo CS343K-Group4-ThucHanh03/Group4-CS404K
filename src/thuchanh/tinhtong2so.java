00abb9538a44a1da3d9c1b10334780c583ea8159
votrannguyen <votrannguyencb@gmail.com>
2021-09-18 15:35:14 +0700

    first commit

---
+++ src/thuchanh/tinhtong2so.java
@@ -0,0 +1,16 @@
+package thuchanh;
+import java.util.*;
+public class tinhtong2so {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap so nguyen a: ");
+		int a = sc.nextInt();
+		System.out.print("Nhap so nguyen b: ");
+		int b = sc.nextInt();
+		int c = a + b;
+		System.out.print("Tong 2 gia tri a va b la " +c);
+	}
+
+}
